1d0ba7c245864a53b8b4c611a2770c561dce17d0
429273-6 <429273-6>
2019-12-04 11:12:06 +0300

    second commit

--- index.php
+++ index.php
@@ -1,21 +1,72 @@
-class A{
+class BuhsovaException extends RuntimeException {
 }
-class B extends A{
-	public a;
-	function __construct($a){
-		$this->a = $a;
-	}
-}
-class C extends B{
-	public b;
-	public c;
-	public d;
-	function __construct($a, $b, $c, $d){
-		parent::__construct($a);
+class LineUr {
+	protected a;
+	protected b;
+	protected x;
+	function __construct($a, $b){
+		$this->a=$a;
 		$this->b=$b;
-		$this->c=$c;
-		$this->d=$d;
+	}
+	function LineUr($a, $b){
+		if ($a != 0) {
+			$x = -1*$b/$a;
+			$this->x = $x;
+			return array($x);
+		}
+		throw new BuhsovaException ("нет решения");	
 	}
 }
-$b = new A();
-$a= new C(new A(), new A(), new B($b), $b);
+class KvadrUr extends LineUr{
+	protected c;
+	protected x2;
+	function __construct($a, $b, $c){
+		parent::__construct($a, $b);
+		$this->c=$c;
+	}
+	protected function dir($a, $b, $c) {
+		$dir = $b*$b - 4*$a*$c;
+		return $dir;
+	}
+	function KvadrUr($a, $b, $c) {
+		if ($a == 0) {
+			return $this->LineUr($a, $b);
+		}
+		$dir = $this->dir();
+		if ($dir > 0) {
+			$x = (-1*$b + sqrt($dir))/(2*$a);
+			$x2 = (-1*$b - sqrt($dir))/(2*$a);
+			$this->x = $x;
+			$this->x2 = $x2;
+			return array($x, $x2);
+		} else ($dir = 0) {
+			$x = (-1*$b)/(2*$a);
+			$this->x = $x;
+			return array($x);
+		}
+		throw new BuhsovaException ("нет решения");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
