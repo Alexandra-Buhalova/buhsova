208622a6e77dcb4b935087834a620cb1d4db79ef
429273-6 <429273-6>
2019-12-10 12:38:01 +0300

    change MyLog

--- buhsova/MyLog.php
+++ buhsova/MyLog.php
@@ -11,7 +11,8 @@ class MyLog extends \core\LogAbstract implements \core\LogInterface
 	{
 		echo implode("\n", $this->log) . "\n";
 		$d = new DateTime();
-		$name="Log/".$d->format("d.m.Y_H.i.s.u").".log";
+		if (!is_dir("/../Log/")) mkdir ("/../Log/");
+		$name="/../Log/".$d->format("d.m.Y_H.i.s.u").".log";
 		file_put_contents($name, implode("\n\r", $this->log) . "\n\r");
 	}
 	public static function write() 
